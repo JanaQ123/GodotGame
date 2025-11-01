b0fb64fae48e4ca972208201e94779ab20d7b985
Jana Qudah <jan20220973@std.psut.edu.jo>
2025-11-01 14:00:23 +0300

    Started Cutscene

--- clover-game-scene/Scripts/BirdScript.gd
+++ clover-game-scene/Scripts/BirdScript.gd
@@ -2,13 +2,8 @@ extends AnimatableBody2D
 
 var can_push := true
 
-func _ready() -> void:
-	# Connect the signal in code to avoid editor connection issues
-	#$Area2D.body_entered.connect(_on_area_2d_body_entered)
-	print("Enemy ready and listening for collisions")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Something entered me:", body.name)
 	if not can_push:
 		return
 	if body.has_method("take_knockback"):
