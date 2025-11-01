efbfcbfe08e7d8fa1828d80f5998a7f6637ef6ad
baraahshq1 <shqairbaraah@gmail.com>
2025-11-01 16:59:58 +0300

    backgroundlooking

--- clover-game-scene/lucywalk.gd
+++ clover-game-scene/lucywalk.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -275.0
 @onready var sprite = $AnimatedSprite2D
 var facing_right = false
 var is_jumping = false
