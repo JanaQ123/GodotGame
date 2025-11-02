32538ec2beb96fb26f8f2510bca9313185998fbb
Layan Tamimi <layanstamimi@gmail.com>
2025-11-02 21:31:38 +0300

    edited tiles, scripts, & clovers

--- clover-game-scene/Scripts/spikes.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var ray_cast: RayCast2D = $RayCast2D
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if ray_cast.is_colliding() and body.get_collider().is_in_group("player"):
-		print("player hit")
-			
--- clover-game-scene/scenes/clover.gd
+++ clover-game-scene/scenes/clover.gd
@@ -1,5 +1,9 @@
-extends Area2D
+extends AnimatableBody2D
 
-func _on_body_entered(body: Node2D) -> void:
+#func _on_body_entered(body: Node2D) -> void:
+	
+
+
+func _on_area_2d_body_entered(body):
 	print("clover collected")
 	queue_free()
