835c300e30a0e4b68b494f60153f7b827e57bae0
Jana Qudah <jan20220973@std.psut.edu.jo>
2025-11-03 15:44:41 +0300

    fixed parkour

--- clover-game-scene/Scripts/Level3playerScript.gd
+++ clover-game-scene/Scripts/Level3playerScript.gd
@@ -18,6 +18,7 @@ var on_wall=false;
 var wall_dir=0;
 var jump_buffer_time = 0.15
 var jump_buffer = 0.0
+var last_wall_dir = 0
 
 func _ready() -> void:
 	$rainbowparent.visible = false   
@@ -71,7 +72,6 @@ func _physics_process(delta: float) -> void:
 		knockback_time -= delta
 		move_and_slide()
 		return
-
 	# 2. Freefall (slow-motion)
 	if freefall:
 		# Optional horizontal control during freefall
@@ -81,15 +81,12 @@ func _physics_process(delta: float) -> void:
 			can_move=true
 			freefall=false
 			$CollisionShape2D.set_deferred("disabled", false)  
-
 		move_and_slide()
 		return
-
 	# 3. Normal movement
 	if can_move:
 		var direction := Input.get_axis("ui_left", "ui_right")
 		velocity.x = direction * SPEED
-
 		# Flip sprite
 		if direction > 0:
 			sprite.flip_h = true
@@ -97,16 +94,13 @@ func _physics_process(delta: float) -> void:
 		elif direction < 0:
 			sprite.flip_h = false
 			facing_right = false
-
 		# Jump input
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 			if powerup:
 				velocity.y = JUMP_VELOCITY * 1.8
-
 			else:
 				velocity.y = JUMP_VELOCITY
 				is_jumping = true
-
 		# Gravity
 		if not is_on_floor():
 			velocity += get_gravity() * delta
@@ -114,14 +108,11 @@ func _physics_process(delta: float) -> void:
 				is_jumping = true
 			else:
 				is_jumping = false
-
 			if not powerup:
 				if velocity.y < 0:
 					is_jumping = true
 				elif is_jumping and is_on_floor():
 					is_jumping = false
-
-
 		# Animation
 		if not is_on_floor():
 			if velocity.y > 400:
@@ -134,26 +125,19 @@ func _physics_process(delta: float) -> void:
 			sprite.play("walk_left")
 		else:
 			sprite.play("idle")
+	on_wall=false
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_collider().is_in_group("wall") and not is_on_floor():
+			on_wall = true
+			wall_dir = -sign(collision.get_normal().x)
 
-		for i in range(get_slide_collision_count()):
-			var collision = get_slide_collision(i)
-			if collision.get_collider().is_in_group("wall"):
-				on_wall = true
-				wall_dir = -sign(collision.get_normal().x)
-				break
-			else:
-				on_wall = false
+	if on_wall:
+		velocity.y = min(velocity.y, 100)
+		if Input.is_action_just_pressed("ui_accept") and wall_dir != last_wall_dir:
+			#velocity.y = min(velocity.y, 100)
+			velocity.x = wall_dir * 300
+			velocity.y = -350
+			last_wall_dir = wall_dir
 
-
-	# Simple wall interaction
-		#if on_wall:
-			#velocity.y = min(velocity.y, 200)
-
-	# Jump
-		if on_wall:
-			var input_dir = Input.get_axis("ui_left", "ui_right")
-			if Input.is_action_just_pressed("ui_accept"):
-				velocity.x = wall_dir * 300
-				velocity.y = -300
-				wall_dir=-wall_dir
-		
+	move_and_slide()
