52b26496fea07f1c3047e95f4e42f9af96caf6ff
Jana Qudah <jan20220973@std.psut.edu.jo>
2025-11-04 13:11:30 +0300

    zoomed cmaera out and added ui

--- clover-game-scene/scenes/dangerZone.gd
+++ clover-game-scene/scenes/dangerZone.gd
@@ -5,7 +5,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("player")):
 		Engine.time_scale=0.7
-		await get_tree().create_timer(0.5).timeout
+		await get_tree().create_timer(0.1).timeout
 		body.get_node("CollisionShape2D").queue_free()
-		await get_tree().create_timer(1.5).timeout
+		await get_tree().create_timer(0.5).timeout
 		get_tree().reload_current_scene()
