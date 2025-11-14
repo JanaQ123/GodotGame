6e6a3f0fffe386e45dcdcb1538dde01e1a6d25f4
Jana Qudah <jan20220973@std.psut.edu.jo>
2025-11-14 15:04:49 +0300

    SomeChanges

--- clover-game-scene/Scripts/pixel_house.gd
+++ clover-game-scene/Scripts/pixel_house.gd
@@ -4,7 +4,7 @@ extends Sprite2D
 
 func _ready() -> void:
 	player.visible = false
-	player.disable_control()
-	await get_tree().create_timer(0.1).timeout
+	#player.disable_control()
+	await get_tree().create_timer(0.8).timeout
 	player.visible = true
 	player.can_move = true
